a3b4141a9b06d3fa3f2d339420dc297da60b3fbb
Nam Le <ltnam2804@gmail.com>
2017-05-10 23:15:56 +0700

    Update install_ubuntu_tweak.sh

--- dotfiles/install_ubuntu_tweak.sh
+++ dotfiles/install_ubuntu_tweak.sh
@@ -2,9 +2,12 @@ echo '------------- install ubuntu tweak ------------------'
 sudo dpkg -i ~/Downloads/ubuntu-tweak_0.8.7-1~getdeb2~xenial_all.deb
 sudo apt-get -f install
 echo '------------- download themes for ubuntu 16.04 -----------'
-sudo add-apt-repository ppa:ravefinity-project/ppa
+sudo add-apt-repository ppa:noobslab/themes
 sudo apt-get update
-sudo apt-get install ambiance-colors radiance-colors
+sudo apt-get install flatabulous-theme
+sudo add-apt-repository ppa:noobslab/icons
+sudo apt-get update
+sudo apt-get install ultra-flat-icons
 echo '------------- download icons for ubuntu 16.04 ------------'
 sudo add-apt-repository ppa:numix/ppa -y
 sudo apt-get update
