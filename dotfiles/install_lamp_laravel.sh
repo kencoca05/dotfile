5bde90dbb679a6bd54ef1c4b28fe9fd0981a299b
Nam Le <ltnam2804@gmail.com>
2019-09-18 09:50:12 +0700

    Update install_lamp_laravel.sh

--- dotfiles/install_lamp_laravel.sh
+++ dotfiles/install_lamp_laravel.sh
@@ -7,7 +7,7 @@ echo "---------------- installing php ---------------------"
 sudo apt-get purge `dpkg -l | grep php| awk '{print $2}' |tr "\n" " "`
 sudo add-apt-repository ppa:ondrej/php
 sudo apt-get update
-sudo apt-get install php7.2 libapache2-mod-php7.2 php7.2-mbstring php7.2-zip php7.2-xml php7.2-mysql php7.2-curl php7.2-mcrypt php7.2-gd
+sudo apt-get install php7.2 libapache2-mod-php7.2 php7.2-mbstring php7.2-zip php7.2-xml php7.2-mysql php7.2-curl php7.2-gd
 echo "---------------- install composer -------------------"
 sudo curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin --filename=composer
 echo "---------------- composer global require ------------"
